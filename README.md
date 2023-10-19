31d04fbf50a549c51879ab651a8615cabec4298b
sikrip <sikrip@gmail.com>
2023-10-19 18:58:00 +0300

    Reading the current fuel table and creating a new one, adjusted based on the logged AFR.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Wot Tuner
 
-Given a csv log from [ApexiPowerTune](https://github.com/sikrip/ApexiPowerTune),
-creates a map of logged AFR to be used for manual fine-tuning the base fuel map under WOT conditions.
+Analyzes ECU logs in order to produce adjusted fuel tables at WOT conditions.
 
-If I find time I will make it semi-automatic so it will provide the new fine-tuned fuel map.
+The existing fuel map is expected in the file 'fuel.map' file and the new map will be 
+produced in the 'new-fuel.map'.
+
+Also, a brief analysis of the ECU logs is printed in the console.
+
+I have tested this with logs from [ApexiPowerTune](https://github.com/sikrip/ApexiPowerTune) but it could be used with
+logs from other ECUs (you will need to adjust the properties accordingly).
 
 ## Usage
 
-> java -jar wot-tuner.jar ecu-log-file1 ecu-log-file2 ...
+Create a properties file (like [this one](./tuner.properties)) and a fuel map file (like [this one](./fuel.map)) and then run:
+
+> java -jar wot-tuner.jar ecu-log-file-path
+
+## Safety notice
+This is experimental, use it with caution and at your own risk!
--- src/main/java/gr/sikrip/WotTuner.java
+++ src/main/java/gr/sikrip/WotTuner.java
@@ -1,15 +1,19 @@
 package gr.sikrip;
 
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.Builder;
 import lombok.ToString;
 
@@ -18,65 +22,182 @@ import lombok.ToString;
  */
 public class WotTuner {
 
-    private static final String ECU_TIME_HEADER = "Time(S)";
-    private static final String ECU_RPM_HEADER = "RPM";
-    private static final String ECU_AFR_HEADER = "WideBand";
-    private static final String ECU_THROTTLE_HEADER = "VTA V";
-    private static final String ECU_MAPN_HEADER = "MAPN";
-    private static final String ECU_MAPP_HEADER = "MAPP";
-    private static final double WOT_VALUE = 4.0;
-    private static final double ACCEL_ENRICH_DURATION_SECONDS = 0.5;
+    private static final String DECIMAL_FORMAT = "%6.3f";
+    private static final String NEW_FUEL_MAP_FILE = "./new-fuel.map";
+    private static int fuelTableSize;
+    private static Integer[] rpmLabels;
+    private static Integer[] loadLabels;
+    private static String timeHeader;
+    private static String rpmHeader;
+    private static String afrHeader;
+    private static String throttleHeader;
+    private static String rpmIdxHeader;
+    private static String loadIdxHeader;
+    private static double wotVolts;
+    private static double accelEnrichSeconds;
+    private static int minNumberOfSamples;
+    private static double wotTargetAfr;
 
     public static void main(String[] args) throws IOException {
         printVersion();
-        if (args.length == 0) {
+        if (args.length != 1) {
             printUsage();
+            return;
         }
-        for (String ecuFilePath : args) {
-            System.out.printf("\nAnalyzing %s\n", ecuFilePath);
-            final List<LogEntry> logEntries = readEcuLog(ecuFilePath);
-            final double[][] loggedAfr = new double[20][20];
-            final int[][] loggedAfrSample = new int[20][20];
-            double wotStart = 0;
-            boolean underWOT = false;
-            for (final LogEntry logEntry : logEntries) {
-                final boolean underWotNow = logEntry.throttle >= WOT_VALUE;
-                if (underWotNow) {
-                    if (!underWOT) {
-                        // Start of wot
-                        wotStart = logEntry.timeSeconds;
-                        underWOT = true;
-                    }
-                } else {
-                    // End of wot
-                    wotStart = 0;
-                    underWOT = false;
-                }
-                if (underWOT) {
-                    if (logEntry.timeSeconds - wotStart >= ACCEL_ENRICH_DURATION_SECONDS) {
-                        // fuel enrichment done
-                        int col = logEntry.mapP - 1;
-                        int row = logEntry.mapN - 1;
-                        final double currentAvgSum =
-                            loggedAfr[col][row] * loggedAfrSample[col][row];
-                        loggedAfrSample[col][row]++;
-                        loggedAfr[col][row] =
-                            (currentAvgSum + logEntry.afr) / loggedAfrSample[col][row];
-                    }
-                }
 
+        final Properties properties = loadProperties();
+        if (properties == null) {
+            return;
+        }
+        loadProperties(properties);
+
+        final double[][] currentFuelMap = readFuelMap();
+        if (currentFuelMap == null) {
+            return;
+        }
+        final String ecuFilePath = args[0];
+        System.out.printf("\nAnalyzing %s...\n", ecuFilePath);
+        final List<LogEntry> logEntries = readEcuLog(ecuFilePath);
+        final double[][] loggedAfr = new double[fuelTableSize][fuelTableSize];
+        final int[][] loggedAfrSample = new int[fuelTableSize][fuelTableSize];
+        double wotStart = 0;
+        boolean underWOT = false;
+        for (final LogEntry logEntry : logEntries) {
+            final boolean underWotNow = logEntry.throttle >= wotVolts;
+            if (underWotNow) {
+                if (!underWOT) {
+                    // Start of wot
+                    wotStart = logEntry.timeSeconds;
+                    underWOT = true;
+                }
+            } else {
+                // End of wot
+                wotStart = 0;
+                underWOT = false;
             }
-            final DecimalFormat decimalFormat = new DecimalFormat("00.00");
-            for (int c = 0; c < loggedAfr.length; c++) {
-                for (int r = 0; r < loggedAfr[c].length; r++) {
-                    System.out.print(decimalFormat.format(loggedAfr[c][r]));
-                    if (r < loggedAfr[c].length - 1) {
+            if (underWOT) {
+                if (logEntry.timeSeconds - wotStart >= accelEnrichSeconds) {
+                    // fuel enrichment done
+                    int col = logEntry.mapP - 1;
+                    int row = logEntry.mapN - 1;
+                    final double currentAvgSum =
+                        loggedAfr[col][row] * loggedAfrSample[col][row];
+                    loggedAfrSample[col][row]++;
+                    loggedAfr[col][row] =
+                        (currentAvgSum + logEntry.afr) / loggedAfrSample[col][row];
+                }
+            }
+
+        }
+
+        System.out.println("\n========= Logged AFR ===========");
+        for (int i = 0; i < loggedAfr.length; i++) {
+            if (i==0) {
+                printMapN_Values();
+            }
+            for (int j = 0; j < loggedAfr[i].length; j++) {
+                if (j==0) {
+                    printMapP_Value(i);
+                }
+                if (loggedAfrSample[i][j] > minNumberOfSamples) {
+                    System.out.printf(DECIMAL_FORMAT, loggedAfr[i][j]);
+                } else {
+                    System.out.printf(DECIMAL_FORMAT, 0.0);
+                }
+                if (j < loggedAfr[i].length - 1) {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        final double[][] newFuelMap = new double[fuelTableSize][fuelTableSize];
+        for (int i = 0; i < loggedAfr.length; i++) {
+            for (int j = 0; j < loggedAfr[i].length; j++) {
+                if (loggedAfrSample[i][j] > minNumberOfSamples) {
+                    final double newFuelValue = (loggedAfr[i][j] / wotTargetAfr) * currentFuelMap[i][j];
+                    newFuelMap[i][j] = newFuelValue;
+                } else {
+                    newFuelMap[i][j] = currentFuelMap[i][j];
+                }
+            }
+        }
+
+        System.out.printf("\n========= New Fuel Map (also saved under %s) ===========\n", NEW_FUEL_MAP_FILE);
+        try (BufferedWriter newFuelMapWriter = new BufferedWriter(new FileWriter(NEW_FUEL_MAP_FILE))) {
+            for (int i = 0; i < newFuelMap.length; i++) {
+                if (i==0) {
+                    printMapN_Values();
+                }
+                for (int j = 0; j < newFuelMap[i].length; j++) {
+                    if (j==0) {
+                        printMapP_Value(i);
+                    }
+                    System.out.printf(DECIMAL_FORMAT, newFuelMap[i][j]);
+                    newFuelMapWriter.write(String.format("%.3f", newFuelMap[i][j]));
+                    if (j < newFuelMap[i].length - 1) {
                         System.out.print("\t");
+                        newFuelMapWriter.write("\t");
                     }
                 }
                 System.out.println();
+                newFuelMapWriter.write("\n");
             }
         }
+        System.out.println("\n========= New-Old Map ===========");
+        for (int i = 0; i < newFuelMap.length; i++) {
+            if (i==0) {
+                printMapN_Values();
+            }
+            for (int j = 0; j < newFuelMap[i].length; j++) {
+                if (j==0) {
+                    printMapP_Value(i);
+                }
+                System.out.printf(DECIMAL_FORMAT, newFuelMap[i][j] - currentFuelMap[i][j]);
+                if (j < newFuelMap[i].length - 1) {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printMapP_Value(int i) {
+        System.out.printf("%5d(%2d)", loadLabels[i], i + 1);
+    }
+
+    private static void printMapN_Values() {
+        System.out.print("\t\t");
+        for(int j = 0; j < fuelTableSize; j++) {
+            System.out.printf("%6d", rpmLabels[j]);
+            if (j < fuelTableSize -1) {
+                System.out.print("\t");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void loadProperties(Properties properties) {
+        rpmLabels = Arrays.stream(properties.getProperty("rpmLabels")
+            .split(","))
+            .map(l -> Integer.parseInt(l.trim()))
+            .toArray(Integer[]::new);
+
+        loadLabels = Arrays.stream(properties.getProperty("loadLabels")
+            .split(","))
+            .map(l -> Integer.parseInt(l.trim()))
+            .toArray(Integer[]::new);
+
+        fuelTableSize = Integer.parseInt(properties.getProperty("fuelTableSize"));
+        timeHeader = properties.getProperty("timeHeader");
+        rpmHeader = properties.getProperty("rpmHeader");
+        afrHeader = properties.getProperty("afrHeader");
+        throttleHeader = properties.getProperty("throttleHeader");
+        rpmIdxHeader = properties.getProperty("rpmIdxHeader");
+        loadIdxHeader = properties.getProperty("loadIdxHeader");
+        wotVolts = Double.parseDouble(properties.getProperty("wotVolt"));
+        accelEnrichSeconds = Double.parseDouble(properties.getProperty("accelEnrichSeconds"));
+        minNumberOfSamples = Integer.parseInt(properties.getProperty("minNumberOfSamples"));
+        wotTargetAfr = Double.parseDouble(properties.getProperty("wotTargetAfr"));
     }
 
     private static void printUsage() {
@@ -92,6 +213,34 @@ public class WotTuner {
         } catch (IOException e) {
             // ignore
         }
+    }
+
+    private static Properties loadProperties() {
+        try (final FileInputStream stream = new FileInputStream("tuner.properties")){
+            final Properties properties = new Properties();
+            properties.load(stream);
+            return properties;
+        } catch (IOException e) {
+            System.err.println("Could not load properties");
+        }
+        return null;
+    }
+
+    private static double[][] readFuelMap() {
+        try (final Stream<String> fileLines = Files.lines(Paths.get("./fuel.map"))) {
+            final double[][] fuelMap = new double[fuelTableSize][fuelTableSize];
+            final List<String> mapLines = fileLines.collect(Collectors.toList());
+            for (int i = 0; i < mapLines.size(); i++) {
+                final String[] mapValues = mapLines.get(i).split("\t");
+                for (int j = 0; j < mapValues.length; j++) {
+                    fuelMap[i][j] = Double.parseDouble(mapValues[j]);
+                }
+            }
+            return fuelMap;
+        } catch (IOException e) {
+            System.err.println("Could not load fuel map");
+        }
+        return null;
     }
 
     private static List<LogEntry> readEcuLog(String filePath) throws IOException {
@@ -117,18 +266,17 @@ public class WotTuner {
                         .build()
                 );
             } else {
-                timeIdx.set(values.indexOf(ECU_TIME_HEADER));
-                rpmIdx.set(values.indexOf(ECU_RPM_HEADER));
-                afrIdx.set(values.indexOf(ECU_AFR_HEADER));
-                throttleIdx.set(values.indexOf(ECU_THROTTLE_HEADER));
-                mapNIdx.set(values.indexOf(ECU_MAPN_HEADER));
-                mapPIdx.set(values.indexOf(ECU_MAPP_HEADER));
+                timeIdx.set(values.indexOf(timeHeader));
+                rpmIdx.set(values.indexOf(rpmHeader));
+                afrIdx.set(values.indexOf(afrHeader));
+                throttleIdx.set(values.indexOf(throttleHeader));
+                mapNIdx.set(values.indexOf(rpmIdxHeader));
+                mapPIdx.set(values.indexOf(loadIdxHeader));
                 headerCreated.set(true);
             }
         });
         return logData;
     }
-
 
     @Builder
     @ToString
